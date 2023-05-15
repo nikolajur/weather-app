3066f6e6330dffac5eb63ccecf85dce4a90fcfc7
Nikola Jurkova <nik.jurkova@gmail.com>
2023-05-15 09:04:14 +0200

    imagery basemap added

--- src/components/Map.js
+++ src/components/Map.js
@@ -12,16 +12,25 @@ const Map = () => {
 
   return (
     <MapContainer center={[49.7, 15.3]} zoom={5}>
-      <TileLayer
-        attribution="&copy;Esri, HERE, Garmin, (c) OpenStreetMap contributors, and the GIS user community"
-        url="https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
-        maxZoom={16}
-      />
       <LayersControl position="topright" collapsed>
         {/*  <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           /> */}
+        <LayersControl.BaseLayer name="canvas" checked>
+          <TileLayer
+            attribution="&copy;Esri, HERE, Garmin, (c) OpenStreetMap contributors, and the GIS user community"
+            url="https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+            maxZoom={16}
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="imagery">
+          <TileLayer
+            attribution="&copy;Esri, Maxar, Earthstar Geographics, and the GIS User Community"
+            url="https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            maxZoom={16}
+          />
+        </LayersControl.BaseLayer>
 
         <LayersControl.Overlay name="radar">
           <TileLayer
@@ -35,6 +44,14 @@ const Map = () => {
         <LayersControl.Overlay name="temperature">
           <TileLayer
             url={"https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=" + key}
+            attribution="OpenWeatherMap"
+            opacity={1}
+          />
+        </LayersControl.Overlay>
+
+        <LayersControl.Overlay name="clouds">
+          <TileLayer
+            url={"https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=" + key}
             attribution="OpenWeatherMap"
             opacity={1}
           />
