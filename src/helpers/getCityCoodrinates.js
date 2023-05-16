a5346dea084ea79fc46225a8b36f1387b867ecb8
Nikola Jurkova <nik.jurkova@gmail.com>
2023-05-16 18:13:41 +0200

    blocked mixed content error fixed

--- src/helpers/getCityCoodrinates.js
+++ src/helpers/getCityCoodrinates.js
@@ -4,7 +4,7 @@ export const getCityCoordinates = async (cityName) => {
   console.log("get weather");
   const key = process.env.REACT_APP_API_KEY;
 
-  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${key}`;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${key}`;
 
   const cityCoordinates = async () => {
     console.log("get city function");
