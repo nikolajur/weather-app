61874d6295d27d33f2763473309d5d958ef68d50
Nikola Jurkova <nik.jurkova@gmail.com>
2023-05-21 15:49:11 +0200

    no layers control

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import LocationContext from "../store/location-context";
-import { MapContainer, LayersControl, LayerGroup, TileLayer } from "react-leaflet";
+import { MapContainer /* , LayersControl, LayerGroup */, TileLayer } from "react-leaflet";
 import LocationGraphics from "./LocationGraphics";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
@@ -8,11 +8,11 @@ import "./Map.css";
 const Map = () => {
   console.log("render map");
   const ctx = useContext(LocationContext);
-  const key = process.env.REACT_APP_API_KEY;
+  // const key = process.env.REACT_APP_API_KEY;
 
   return (
     <MapContainer center={[49.7, 15.3]} zoom={5}>
-      <LayersControl position="topright" collapsed>
+      {/* <LayersControl position="topright" collapsed>
         <LayersControl.BaseLayer name="basemap" checked>
           <TileLayer
             attribution='&copy;&nbsp;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy;&nbsp;<a href="https://carto.com/attributions">CARTO</a>'
@@ -37,8 +37,12 @@ const Map = () => {
             />
           </LayerGroup>
         </LayersControl.BaseLayer>
-      </LayersControl>
-
+      </LayersControl> */}
+      <TileLayer
+        attribution='&copy;&nbsp;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy;&nbsp;<a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        maxZoom={20}
+      />
       {ctx.coordinates && (
         <>
           <LocationGraphics />
